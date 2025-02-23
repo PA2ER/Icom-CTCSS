477453dd3d635786e0c6261bdcfc073a8087140c
Erik Rothuis <97060889+PA2ER@users.noreply.github.com>
2025-02-23 21:47:49 +0100

    V0.02 23-02-2024

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@ I used the library written by PE1CID,that I slightly modified, to generate the s
 Pins used:
 PIN 2 = PTT_BUTTON to switch from receive to transmit
 PIN 3 = PWM_OUTPUT output that generates the sub-audio tone
-PIN 4 = latchPin Input of the latch from the transceiver
 PIN 8 = DECODE_INDICATOR goes high if CTCSS code is decoded
+PIN 10 = latchPin Input of the latch from the transceiver
 PIN 11 = MOSI SPI data from transceiver
 PIN 13 = SCK SPI clock serial clock from transceiver
 PIN 14 = A0 Sub-audio tone input, needs 260Hz lowpass filter
@@ -28,7 +28,9 @@ bool decodeCTCSS(float targetFrequency);
 
 // PTT Button
 #define PTT_BUTTON 2  // Pin connected to the PTT button
-const int latchPin = 4;  // STCP (Storage Register Clock Input) - Latch signal
+#define latchPin 4  // STCP (Storage Register Clock Input) - Latch signal
+#define SCK_PIN   13  // D13 = pin19 = PortB.5
+#define MOSI_PIN  11  // D11 = pin17 = PortB.3
 
 // Decode Indicator Output
 #define DECODE_INDICATOR 8  // Pin for decode indicator (e.g., LED)
@@ -38,14 +40,19 @@ const int latchPin = 4;  // STCP (Storage Register Clock Input) - Latch signal
 
 // TIA-603-E CTCSS Frequencies (Hz)
 const float ctcssFrequencies[] = {
-  00.0, 67.0, 71.9, 74.4, 77.0, 79.7, 82.5, 85.4, 88.5, 91.5, 94.8, 97.4, 100.0, 103.5, 107.2, 110.9, 114.8
+  00.0, 67.0, 71.9, 74.4, 77.0, 79.7, 82.5, 85.4, 88.5, 91.5, 94.8, 100.0, 103.5, 107.2, 110.9, 114.8
   , 118.8, 123.0, 127.3, 131.8, 136.5, 141.3, 146.2, 151.4, 156.7, 162.2, 167.9, 173.8, 179.9, 186.2, 192.8
   , 203.5, 210.7, 218.1, 225.7, 233.6, 241.8, 250.3
 };
 
-const uint8_t ctcssCodes[] = {
+/*const uint8_t ctcssCodes[] = {
   0, 29, 28, 27, 26, 25, 24, 23, 22, 21, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42
   , 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30
+};*/
+
+const uint8_t ctcssCodes[] = {
+  192, 157, 156, 155, 154, 153, 152, 151, 150, 149, 185, 184, 183, 182, 181, 180, 179, 178, 177, 176, 175, 174, 173, 172, 171, 170, 169
+  , 168, 167, 166, 165, 164, 163, 162, 161, 160, 159, 158
 };
 
 // Initialize all bits to 0
@@ -71,17 +78,20 @@ int numCodes = sizeof(ctcssCodes) / sizeof(ctcssCodes[0]);
 
 void setup() {
   // Latch pin is an INPUT
-  pinMode(latchPin, INPUT_PULLUP); 
+  pinMode(latchPin, INPUT); 
+  pinMode(SCK_PIN, INPUT);
+  pinMode(MOSI_PIN, INPUT);
 
   // Initialize SPI in slave mode
-  SPCR |= _BV(SPE); // Enable SPI in slave mode
-  SPCR |= _BV(SPIE); // Enable SPI interrupt
+  //SPCR = (1<<SPE)|(0<<DORD)|(0<<MSTR)|(1<<CPOL)|(0<<CPHA)|(1<<SPR1)|(1<<SPR0); // SPI on
+  //SPCR |= _BV(SPE); // Enable SPI in slave mode
+  //SPCR |= _BV(SPIE); // Enable SPI interrupt
 
   TCCR1A = _BV(COM1A1) | _BV(WGM10);  // Set PWM mode and output
   TCCR1B = _BV(CS12);  // Set prescaler to 1 (no prescaling)
 
   // Initialize Serial Monitor for debugging (optional)
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   // Initialize PTT button as input
   pinMode(PTT_BUTTON, INPUT);
@@ -106,8 +116,8 @@ void loop() {
   // Check if the PTT button is pressed
   if (digitalRead(PTT_BUTTON) == HIGH && selectedCode != 0 ) {
     // PTT button is pressed - transmit selected CTCSS tone as a sine wave
-    digitalWrite(DECODE_INDICATOR, LOW);  // Turn off decode indicator
-    generateSineWave(selectedCode);
+      digitalWrite(DECODE_INDICATOR, LOW);  // Turn off decode indicator
+      generateSineWave(selectedCode);
     if (selectedCode != prefCode) {
       Serial.print("Transmitting CTCSS tone: ");
       Serial.println(ctcssFrequencies[selectedCode]);
@@ -115,7 +125,7 @@ void loop() {
   } else {
     // PTT button is not pressed - start decode incoming audio
     if (selectedCode != 0) {
-      bool isDecoded = decodeCTCSS(ctcssFrequencies[selectedCode - 1]);
+      bool isDecoded = decodeCTCSS(ctcssFrequencies[selectedCode]);
       digitalWrite(DECODE_INDICATOR, isDecoded ? HIGH : LOW);  // Turn on/off decode indicator
     } else {
       digitalWrite(DECODE_INDICATOR, LOW);  // Turn off decode indicator if no code is selected
@@ -124,22 +134,28 @@ void loop() {
 }
 
 // SPI interrupt service routine
-ISR(SPI_STC_vect) {
+/*ISR(SPI_STC_vect) {
   // Read the received data from the SPI Data Register (SPDR)
   shiftRegister = SPDR;
 
   // Print the received data to the Serial Monitor for debugging (optional)
   //Serial.print("Received Data: ");
   //Serial.println(shiftRegister, DEC);
-}
+}*/
 
 int readShiftRegister() {
-  if (digitalRead(latchPin) == LOW) {
+  SPCR = (1<<SPE)|(0<<DORD)|(0<<MSTR)|(1<<CPOL)|(1<<CPHA)|(1<<SPR1)|(1<<SPR0); // SPI on
+  while(!(SPSR & (1<<SPIF))) ; // SPIF bit set when 8 bits received
+  shiftRegister = SPDR;
+  SPCR = (0<<SPE)|(0<<DORD)|(0<<MSTR)|(1<<CPOL)|(1<<CPHA)|(1<<SPR1)|(1<<SPR0);  // SPI off
+  while (digitalRead(SS)==0) {} // wait until SlaveSelect goes low (active)
+  int code = 0;
+  if (shiftRegister >= 0) {
     for (int i =0; i < numCodes; i++) {
       if (shiftRegister == ctcssCodes[i]) {
         code = i;
-        //Serial.print("Code: ");
-        //Serial.println(code, DEC);
+        Serial.print("Code: ");
+        Serial.println(shiftRegister, DEC);
         break;
       }
     }
